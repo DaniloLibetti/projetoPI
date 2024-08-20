96624492844e08ff76f2a32aadb8922ff8cf9d03
Turith1 <162712980+Turith1@users.noreply.github.com>
2024-08-20 19:33:35 -0300

    New Scene with character moving

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,7 @@ public class PlayerMovement : MonoBehaviour
 {
 
     [SerializeField] Rigidbody playerRb;
+    [SerializeField] Animator _charAnim;
 
     public float speed = 5f;
 
@@ -24,5 +25,29 @@ public class PlayerMovement : MonoBehaviour
         Vector3 movement = new Vector3(horizontal, 0f, 0f) * speed * Time.deltaTime;
 
         playerRb.MovePosition(transform.position + movement);
+
+        switch (horizontal)
+        {
+            case < -.01f:
+                gameObject.transform.rotation = Quaternion.Euler(0, 180, 0);
+                _charAnim.SetBool("Walk", true);
+                break;
+            case > .01f:
+                gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
+                _charAnim.SetBool("Walk", true);
+                break;
+            default:
+                _charAnim.SetBool("Walk", false);
+                break;
+        }
+
+        //if(horizontal < -.01f || horizontal > .01f)
+        //{
+        //    _charAnim.SetBool("Walk", true);
+        //}
+        //else if(horizontal == 0)
+        //{
+        //    _charAnim.SetBool("Walk", false);
+        //}
     }
 }
