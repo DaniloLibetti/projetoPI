ced45e3301de6839794ae2c36736f998936d0420
Danilo Libetti Lasse Silva <danilolibetti@gmail.com>
2024-10-25 17:02:19 -0300

    Material Coletavel adicionado

---
+++ Assets/Scripts/EnemyScripts/CoinDrop.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinDrop : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _CollectableMaterial;
+    [SerializeField]
+    private Transform _transform;
+    [SerializeField]
+    private Health _enemyHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(_enemyHealth.GetComponent<Health>()._health <= 0)
+        {
+            DropCoin();
+        }
+    }
+
+    private void DropCoin()
+    {
+        Vector3 position = transform.position;
+        GameObject mat = Instantiate(_CollectableMaterial, position, Quaternion.identity);
+    }
+}
