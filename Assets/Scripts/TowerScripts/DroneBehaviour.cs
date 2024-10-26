45bfaab723125727a9a69798544b6a24d50e46bb
Turith1 <162712980+Turith1@users.noreply.github.com>
2024-10-25 21:06:44 -0300

    Drone Done

--- Assets/Scripts/TowerScripts/DroneBehaviour.cs
+++ Assets/Scripts/TowerScripts/DroneBehaviour.cs
@@ -10,26 +10,36 @@ public class DroneBehaviour : MonoBehaviour
     private float _currentX = 0;
     private float _pingpongHeight = 1;
     private float _pingpongX = 1;
+    private float _attackSpeed;
     private Vector3 _start;
     [SerializeField]
     private float _damage;
     [SerializeField]
     private Vector3 _target;
     [SerializeField]
-    private Transform _drone;
-    [SerializeField]
     private AnimationCurve _heightCurve;
     [SerializeField]
     private AnimationCurve _idleCurve;
-    ///[SerializeField]
-    ///private AnimationCurve _idleCurve;
-    private bool _topHeight;
+    [SerializeField]
+    private AnimationCurve _attackCurve;
+    [SerializeField]
+    private Transform _lockedEnemy;
+    [SerializeField]
+    private float _timer;
+    private float _minDist = Mathf.Infinity;
+    public LayerMask m_LayerMask;
+    public bool _topHeight;
+    public bool _attack = false;
     private bool _firstRound = false;
+    public DroneLauncher _launcher;
+
+    public Transform _target1, _target2, _currentTarget;
 
     // Start is called before the first frame update
     void Start()
     {
-        _start = transform.position;
+        _currentTarget = _target1;
+        _start = this.transform.position;
         StartCoroutine(DroneHeight());
     }
 
@@ -48,20 +58,62 @@ public class DroneBehaviour : MonoBehaviour
             }
             _currentHeight = Mathf.MoveTowards(_currentHeight, _pingpongHeight, 1f * Time.deltaTime);
             _currentX = Mathf.PingPong(.1f * Time.time, 1);
-            transform.position = Vector3.Lerp(new Vector3(transform.position.x, 10, _start.z), new Vector3(transform.position.x, 12, _start.z), _idleCurve.Evaluate(_currentHeight));
-            transform.position = Vector3.Lerp(new Vector3(_start.x + _t, transform.position.y, _start.z), new Vector3(_start.x - 10, transform.position.y, _start.z), _idleCurve.Evaluate(_currentX));
+            this.transform.position = Vector3.Lerp(new Vector3(transform.position.x, 10, _start.z), new Vector3(transform.position.x, 12, _start.z), _idleCurve.Evaluate(_currentHeight));
+            this.transform.position = Vector3.Lerp(new Vector3(_start.x + _t, transform.position.y, _start.z), new Vector3(_start.x - 10, transform.position.y, _start.z), _idleCurve.Evaluate(_currentX));
             if(transform.position.x <= _start.x - 5 && _firstRound == false)
             {
                 _t = 10;
                 _firstRound = true;
             }
         }
+        else if(_attack == true && (_lockedEnemy != null && _lockedEnemy.gameObject.activeInHierarchy))
+        {
+            _attackSpeed = Mathf.MoveTowards(0, 1, 5f * Time.deltaTime);
+            transform.position = Vector3.Lerp(transform.position, _lockedEnemy.position, _attackCurve.Evaluate(_attackSpeed));
+        }
+    }
 
+    void FixedUpdate()
+    {
+        if(_topHeight == true && (_lockedEnemy == null || !_lockedEnemy.gameObject.activeInHierarchy))
+        {
+            _timer += Time.deltaTime;
+            if (_timer >= 2)
+            {
+                if(_attack == false)
+                    DetectEnemy();
+                _timer = 0;
+            }
+        }
+    }
+
+    private void DetectEnemy()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, 50, m_LayerMask);
+        Debug.DrawLine(transform.position, new Vector3(transform.position.x + 50, transform.position.y, transform.position.z), Color.red, 2);
+        int i = 0;
+
+        if (hitColliders.Length > 0)
+        {
+            while (i < hitColliders.Length)
+            {
+                float dist = Vector3.Distance(hitColliders[i].transform.position, transform.position);
+                if (dist < _minDist && hitColliders[i].gameObject.activeInHierarchy)
+                {
+                    _lockedEnemy = hitColliders[i].transform;
+                    _minDist = dist;
+                    _topHeight = false;
+                    _attack = true;
+                }
+                i++;
+            }
+            _minDist = Mathf.Infinity;
+        }
     }
 
 
 
-    private IEnumerator DroneHeight()
+    public IEnumerator DroneHeight()
     {
 
         while (_t < 1)
@@ -77,5 +129,17 @@ public class DroneBehaviour : MonoBehaviour
         _topHeight = true;
         _t = 0;
         yield return null;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("AirEnemy"))
+        {
+            other.GetComponent<Health>().ReceiveDamage(70);
+            _lockedEnemy = null;
+            _attack = false;
+            _launcher.Revive(this.gameObject);
+            this.gameObject.SetActive(false);
+        }
     }
 }
--- Assets/Scripts/TowerScripts/DroneLauncher.cs
+++ Assets/Scripts/TowerScripts/DroneLauncher.cs
@@ -8,15 +8,47 @@ public class DroneLauncher : MonoBehaviour
     private GameObject _drone;
     [SerializeField]
     private float _launchate;
+    [SerializeField]
+    private Transform _target1, _target2;
 
     void Start()
     {
-        Invoke("Launch", 1);
+        StartCoroutine(CallDrones());
+        //Invoke("Launch", 1);
     }
 
     private void Launch()
     {
-        Instantiate(_drone, new Vector3(transform.position.x, transform.position.y + 2, transform.position.z), Quaternion.identity);
-        Invoke("Launch", _launchate);
+        GameObject drone = Instantiate(_drone, new Vector3(transform.position.x, transform.position.y + 2, transform.position.z), Quaternion.identity);
+        DroneBehaviour targets = drone.GetComponent<DroneBehaviour>();
+        targets._launcher = this.GetComponent<DroneLauncher>();
+        targets._target1 = _target1;
+        targets._target2 = _target2;
+        ///Invoke("Launch", _launchate);
+    }
+
+    IEnumerator CallDrones()
+    {
+        yield return new WaitForSeconds(1);
+        Launch();
+        yield return new WaitForSeconds(3);
+        Launch();
+    }
+
+    public void Revive(GameObject drone)
+    {
+        StartCoroutine(Reactivate(drone));
+    }
+
+    public IEnumerator Reactivate(GameObject drone)
+    {
+        Debug.Log(drone.name);
+        drone.transform.position = new Vector3(transform.position.x, transform.position.y + 5, transform.position.z);
+        yield return new WaitForSeconds(2);
+        DroneBehaviour targets = drone.GetComponent<DroneBehaviour>();
+        targets._topHeight = false;
+        targets._attack = false;
+        StartCoroutine(targets.DroneHeight());
+        drone.SetActive(true);
     }
 }
