f74bc73f6e21701678b76f951950bb3d0046d88e
Turith1 <162712980+Turith1@users.noreply.github.com>
2024-11-19 16:17:58 -0300

    Waves version 1

--- Assets/Scripts/EnemyScripts/EnemySpawner.cs
+++ Assets/Scripts/EnemyScripts/EnemySpawner.cs
@@ -11,68 +11,93 @@ public class EnemySpawner : MonoBehaviour
     private List<GameObject> _ground = new();
     [SerializeField]
     private List<GameObject> _flying = new();
+    [SerializeField]
+    private float _time = 25f;
+    private int _leftRightSpawn = 250;
+    [SerializeField]
+    private int _enemyCount = 10;
 
     // Primeira onda devera ser apenas um inimgo da direita, segunda um da direita e outro da esquerda
     //Ondas subsequentes deverão iniciar com 5 inimigos de cada lado e depois onda anterior * 2
     void Start()
     {
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i <= 10; i++)
         {
             GameObject enemy = Instantiate(_enemies[0], new Vector3(transform.position.x, _enemyHeight[0], transform.position.z), Quaternion.identity);
             enemy.SetActive(false);
             _ground.Add(enemy);
         }
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i <= 10; i++)
         {
             GameObject enemy = Instantiate(_enemies[1], new Vector3(transform.position.x, _enemyHeight[1], transform.position.z), Quaternion.identity);
             enemy.SetActive(false);
             _flying.Add(enemy);
         }
-        Spawn();
+    }
+
+    private void Update()
+    {
+        _time -= Time.deltaTime;
+        if(_time <= 0)
+        {
+            Spawn();
+            _time = 60;
+        }
     }
 
     private void Spawn()
     {
-        //chooses between flying or grounded
-        int correctEnemy = Random.Range(0, 2);
-
-        //verifies pool for grounded enemies, instantiates if they are not found in list
-        if(correctEnemy == 0)
+        for(int i = 0; i < _enemyCount; i++)
         {
+            if(i == ((_enemyCount / 2) - 1))
+            {
+                _leftRightSpawn = -250;
+            }
+            float timer = Random.Range(4, 8);
+            StartCoroutine(SpawnGround(timer, _leftRightSpawn));
+            StartCoroutine(SpawnAir(timer, _leftRightSpawn));
+        }
+        _enemyCount *= 2;
+        _leftRightSpawn = 250;
+    }
+
+    IEnumerator SpawnGround(float waitTime, int side)
+    {
+        yield return new WaitForSeconds(waitTime);
             GameObject groundEnemy = GetGroundEnemies();
             if(groundEnemy != null)
             {
-                groundEnemy.transform.position = new Vector3(transform.position.x, _enemyHeight[0], transform.position.z);
+                groundEnemy.transform.position = new Vector3(side, _enemyHeight[0], transform.position.z);
                 groundEnemy.GetComponent<Health>()._health = 100;
                 groundEnemy.SetActive(true);
             }
             else
             {
-                GameObject enemy = Instantiate(_enemies[0],new Vector3(transform.position.x, _enemyHeight[0], transform.position.z) , Quaternion.identity);
+                GameObject enemy = Instantiate(_enemies[0],new Vector3(side, _enemyHeight[0], transform.position.z) , Quaternion.identity);
                 enemy.SetActive(false);
                 _ground.Add(enemy);
+                StartCoroutine(SpawnGround(waitTime, side));
             }
-        }
-        //verifies pool for flying enemies, instantiates if they are not found
-        else if(correctEnemy == 1)
+    }
+
+    IEnumerator SpawnAir(float waitTime, int side)
+    {
+        yield return new WaitForSeconds(waitTime);
+        GameObject AirEnemy = GetFlyingEnemies();
+        if (AirEnemy != null)
         {
-            GameObject groundEnemy = GetFlyingEnemies();
-            if (groundEnemy != null)
-            {
-                groundEnemy.transform.position = new Vector3(transform.position.x, _enemyHeight[1], transform.position.z);
-                groundEnemy.GetComponent<Health>()._health = 100;
-                groundEnemy.SetActive(true);
-            }
-            else
-            {
-                GameObject enemy = Instantiate(_enemies[1], new Vector3(transform.position.x, _enemyHeight[1], transform.position.z), Quaternion.identity);
-                enemy.SetActive(false);
-                _flying.Add(enemy);
-            }
+            AirEnemy.transform.position = new Vector3(_leftRightSpawn, _enemyHeight[1], transform.position.z);
+            AirEnemy.GetComponent<Health>()._health = 100;
+            AirEnemy.SetActive(true);
+        }
+        else
+        {
+            GameObject enemy = Instantiate(_enemies[1], new Vector3(_leftRightSpawn, _enemyHeight[1], transform.position.z), Quaternion.identity);
+            enemy.SetActive(false);
+            _flying.Add(enemy);
+            StartCoroutine(SpawnAir(waitTime, side));
         }
 
-        float time = Random.Range(1, 3);
-        Invoke("Spawn", time);
     }
 
     public GameObject GetGroundEnemies()
